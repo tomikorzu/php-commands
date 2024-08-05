b431f1107020961753779af2e62abd4742f06432
tomikorzu <tomykorzu@icloud.com>
2024-08-04 21:30:53 -0300

    feat(index.php / cuestionario.txt / instalation/txt): add those content

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    $title = 'Mundo'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    <div class="welcome-message">
+        <h1>Hola <?php echo $title ?></h1>
+    </div>
+</body>
+</html>
